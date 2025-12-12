af077e92522e3ddf76f8bac96dffc55f2ab57ec4
Gabriel Marangoni <88441780+marang4@users.noreply.github.com>
2025-12-11 21:05:45 -0300

    Remove unused React import in Index.tsx

--- latest/src/pages/historico/Index.tsx
+++ latest/src/pages/historico/Index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { useSelector, useDispatch } from 'react-redux';
 import type { RootState } from '../../store/store';
 import { clearHistory } from '../../store/historySlice';
